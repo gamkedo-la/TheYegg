7efd6e62d7fd320d5e9e5a0225762203936fb942
Antti Lipsanen <a.lipsanen@gmail.com>
2021-08-10 19:26:37 +0300

    Added navmesh pathfinding

--- Assets/Scripts/NPC/State Machines/GuardStatePatrol.cs
+++ Assets/Scripts/NPC/State Machines/GuardStatePatrol.cs
@@ -1,22 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GuardStatePatrol : GuardState
 {
 
+    [Header("Patrol pathfinding settings")]
     [SerializeField] List<Transform> patrolPoints;
+    [SerializeField] float allowedDistanceFromPoint = .5f;
+    [SerializeField] float patrolSpeed = 5f;
+    [Header("Required components")] 
+    [SerializeField] NavMeshAgent navMeshAgent;
+
+
+    //private
+    private int currentPatrolPointIndex;
 
     public override void StartGuardState()
     {
         base.StartGuardState();
+        navMeshAgent.autoBraking = false;
+        navMeshAgent.speed = patrolSpeed;
+        GoToNextPatrolPoint();
+    }
+
+    private void GoToNextPatrolPoint()
+    {
+        if(patrolPoints.Count == 0){
+            return;
+        }
+
+        navMeshAgent.destination = patrolPoints[currentPatrolPointIndex].position;
+
+        currentPatrolPointIndex = (currentPatrolPointIndex + 1) % patrolPoints.Count;
     }
 
     public override void RunGuardState()
     {
         base.RunGuardState();
-        //Debug.Log(name + " is patrolling");
         //TODO create pathfinding between patrolpoints so that we can set random patrolpoints when player is detected
+        if(!navMeshAgent.pathPending && navMeshAgent.remainingDistance < allowedDistanceFromPoint){
+            GoToNextPatrolPoint();
+        }
     }
 
     public override void EndGuardState()
@@ -26,7 +53,12 @@ public class GuardStatePatrol : GuardState
         
     }
 
-
-
-
+    private void OnCollisionStay(Collision other) {
+        if(other.gameObject.CompareTag("Floor")){
+            Debug.Log("Guard is on floor");
+            navMeshAgent.enabled = false;
+            navMeshAgent.enabled = true;                
+        }
+    }
 }
+
