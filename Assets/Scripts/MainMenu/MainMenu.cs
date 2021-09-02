490112abc8b7dc10c46dba94976eb13ae3d27a4a
TheHaad <fmuntaz@gmail.com>
2021-09-02 12:11:15 -0400

    Add Main Menu

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    [Header("Required Starting Scene")]
+    [SerializeField] public string startScenePath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(startScenePath, LoadSceneMode.Additive);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
