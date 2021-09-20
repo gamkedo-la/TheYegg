9c4a0a6b587ff84770e135ce2fc4c83feb0e329b
Antti Lipsanen <a.lipsanen@gmail.com>
2021-09-20 20:12:57 +0300

    Added AudioController, in-game menu and audiomixer

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioController : MonoBehaviour
+{
+    
+    //contains all the required public methods to control volume in the in game menu
+
+    public AudioMixer audioMixer;
+    [SerializeField] InGameMenuHandler inGameMenuHandler;
+
+    private void Start() {
+        inGameMenuHandler = FindObjectOfType<InGameMenuHandler>();
+        if(!inGameMenuHandler || inGameMenuHandler == null){
+            Debug.Log("No in game menu handler found in Audio Controller!");
+        }
+    }
+
+    public void SetMasterVolume(Slider volume){
+        audioMixer.SetFloat("masterVolume", volume.value / 100f * -80f);
+        inGameMenuHandler.SetMasterVolumeValue(100 - Mathf.RoundToInt(volume.value));
+    }
+
+    public void SetSFXVolume(Slider volume){
+        audioMixer.SetFloat("sfxVolume", volume.value / 100f * -80f);
+        inGameMenuHandler.SetSFXVolumeValue(100 - Mathf.RoundToInt(volume.value));
+    }
+
+    public void SetMusicVolume(Slider volume){
+        audioMixer.SetFloat("musicVolume", volume.value / 100f * -80f);
+        inGameMenuHandler.SetBGVolumeValue(100 - Mathf.RoundToInt(volume.value));
+    }
+
+}
---
+++ Assets/Scripts/InGameMenuHandler.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InGameMenuHandler : MonoBehaviour
+{
+    
+    [SerializeField] GameObject menuPanel;
+    [SerializeField] KeyCode mainMenuInput;
+
+    [SerializeField] TextMeshProUGUI masterVolumeText;
+    [SerializeField] TextMeshProUGUI sfxVolumeText;
+    [SerializeField] TextMeshProUGUI backgroundVolumeText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get main menu update
+        if(Input.GetKeyDown(mainMenuInput)){
+            ToggleMenuPanel();
+        }
+    }
+
+    public void ToggleMenuPanel(){
+        menuPanel.SetActive(!menuPanel.activeSelf);
+    }
+
+    public void SetMasterVolumeValue(int value){
+        masterVolumeText.text = value.ToString();
+    }
+
+    public void SetSFXVolumeValue(int value){
+        sfxVolumeText.text = value.ToString();
+    }
+
+    public void SetBGVolumeValue(int value){
+        backgroundVolumeText.text = value.ToString();
+    }
+
+
+    
+}
