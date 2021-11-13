7b05d0f69c1ecd4602f53ab38edc13a16c627e19
Antti Lipsanen <a.lipsanen@gmail.com>
2021-11-13 21:21:13 +0200

    Added industrial level to level flow

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -5,21 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
-    [Header("Required Starting Scene")]
-    [SerializeField] public string startScenePath;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void startGame()
     {
         //SceneManager.LoadScene(startScenePath, LoadSceneMode.Single);
