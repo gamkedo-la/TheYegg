3fe87375664e7d6b0db627167a8c5a702c8486d0
Antti Lipsanen <a.lipsanen@gmail.com>
2021-11-28 12:58:07 +0200

    Added in-game instructions for controls

--- Assets/Scripts/MiniMap.cs
+++ Assets/Scripts/MiniMap.cs
@@ -6,15 +6,10 @@ public class MiniMap : MonoBehaviour
 {
     public bool Enabled = false;
     public GameObject theMap;
-    
-    void Start()
-    {
-        
-    }
 
     void Update()
     {
-         if (Input.GetKeyDown(KeyCode.M)) {
+         if (Input.GetKeyDown(KeyCode.V)) {
              Enabled = !Enabled; // toggle
              Debug.Log("MiniMap is "+(Enabled?"ON":"OFF"));
              if (theMap) theMap.SetActive(Enabled);
