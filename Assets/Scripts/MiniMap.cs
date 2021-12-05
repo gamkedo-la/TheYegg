a2c1fd00eded10db4ed0132d79a25d9429c214e5
Antti Lipsanen <a.lipsanen@gmail.com>
2021-12-05 08:39:54 +0200

    Minimap is on by default

--- Assets/Scripts/MiniMap.cs
+++ Assets/Scripts/MiniMap.cs
@@ -7,12 +7,23 @@ public class MiniMap : MonoBehaviour
     public bool Enabled = false;
     public GameObject theMap;
 
+    private void Start() {
+        SetMiniMapActive(true);
+    }
+
+
     void Update()
     {
-         if (Input.GetKeyDown(KeyCode.V)) {
-             Enabled = !Enabled; // toggle
-             Debug.Log("MiniMap is "+(Enabled?"ON":"OFF"));
-             if (theMap) theMap.SetActive(Enabled);
-         }
+        if (Input.GetKeyDown(KeyCode.V)) {
+            SetMiniMapActive(!Enabled);
+            
+        }
+    }
+
+    private void SetMiniMapActive(bool t)
+    {
+        Enabled = t;
+        Debug.Log("MiniMap is "+(Enabled?"ON":"OFF"));
+        if (theMap) theMap.SetActive(Enabled);
     }
 }
